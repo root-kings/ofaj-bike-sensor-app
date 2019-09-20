4591f559af4dd24545f721b9d68c7831cb723504
krushndayshmookh <krushndayshmookh@gmail.com>
2019-09-20 14:22:50 +0530

    design login component

---
+++ src/components/Login.vue
@@ -0,0 +1,35 @@
+<template lang="pug">
+  #login.pink.lighten-5.screen-height.valign-wrapper
+    .full-width
+      .row
+        .col.s12.m6.offset-m3.l4.offset-l4
+          .card-panel
+            h4.light.center fuelostic
+            h5.light.center Login
+
+            .row
+              .input-field.col.s12
+                input#username.validate(type="text")
+                label(for="username") Username
+            
+              .input-field.col.s12
+                input#password.validate(type="password")
+                label(for="password") Password
+            
+            .row
+              .col.s12
+                .btn-large.full-width.blue Login
+
+            .row
+              .col.s12.center
+                .btn-flat.full-width(@click="") Forgot Password
+
+</template>
+
+<script>
+export default {
+  name: 'Login'
+}
+</script>
+<!-- styling for the component -->
+<style></style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import About from '@/components/About'
+import Login from '@/components/Login'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/about',
       name: 'About',
       component: About
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
     }
   ],
   mode: 'history'
