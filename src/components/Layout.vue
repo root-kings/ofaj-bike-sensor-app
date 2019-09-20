5ab7023721d93a979ac328fad330e30f4121c7c7
krushndayshmookh <krushndayshmookh@gmail.com>
2019-09-20 14:35:01 +0530

    layout created

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -2,26 +2,50 @@
   #layout
     header
       nav
-        .nav-wrapper
+        .nav-wrapper.blue-grey
           a.brand-logo(href="#") Logo
+          a.sidenav-trigger(href="#" data-target="mobile-demo")
+            i.material-icons menu
           ul#nav-mobile.right.hide-on-med-and-down
             li
-              a(href="sass.html") Sass
+              router-link(:to="'/vehicles'") Vehicles              
             li
-              a(href="badges.html") Components
-            li
-              a(href="collapsible.html") JavaScript
+              router-link(:to="'/profile'") Profile
+      ul#mobile-demo.sidenav
+        li
+          router-link(:to="'/vehicles'") Vehicles              
+        li
+          router-link(:to="'/profile'") Profile
+
     main
       slot
-    footer
+    // footer
 </template>
 
 <script>
 export default {
-  name: 'Layout'
+  name: 'Layout',
+  mounted: function() {
+    M.AutoInit()
+  }
 }
 </script>
 <!-- styling for the component -->
 <style>
+@media only screen and (min-width: 992px) {
+  .brand-logo {
+    margin-left: 15px;
+  }
+}
+nav .brand-logo img {
+  max-height: 48px;
+}
 
+nav,
+nav .nav-wrapper i,
+nav a.sidenav-trigger,
+nav a.sidenav-trigger i {
+  height: 48px;
+  line-height: 48px;
+}
 </style>
