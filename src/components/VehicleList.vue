a82d510093b767d70d4b583065bcb0397031e55d
krushndayshmookh <krushndayshmookh@gmail.com>
2019-09-20 16:02:14 +0530

    create vehiclelist comp

---
+++ src/components/VehicleList.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+  .collection
+    a.collection-item(v-for="vehicle in vehicles", :key="vehicle._id") {{ vehicle.make }} {{ vehicle.model }}
+</template>
+
+<script>
+export default {
+  name: 'VehicleList',
+  props: ['vehicles'],
+  mounted: () => {
+    console.log(this.props)
+  }
+}
+</script>
+<!-- styling for the component -->
+<style></style>
