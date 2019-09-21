7aa4266fa7b69548a28f2d58b8a027faa6a17d0a
krushndayshmookh <krushndayshmookh@gmail.com>
2019-09-21 14:46:41 +0530

    client side session management using vue-session

--- src/main.js
+++ src/main.js
@@ -1,13 +1,18 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import VueSession from 'vue-session'
+
 import App from './App'
 import router from './router'
+
 import 'materialize-css'
 import 'materialize-css/dist/css/materialize.css'
 import './assets/css/dayshmookh.layout.css'
 
 Vue.config.productionTip = false
+
+Vue.use(VueSession)
 
 /* eslint-disable no-new */
 new Vue({
